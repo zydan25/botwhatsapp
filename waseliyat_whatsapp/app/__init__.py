5e7993161a7e8fe64056c6339dd5c67b94f6f0ff
zydan25 <zydanalattab25w@gmail.com>
2026-09-13 20:44:32 +0300

    Register Takhfid chat blueprint

--- waseliyat_whatsapp/app/__init__.py
+++ waseliyat_whatsapp/app/__init__.py
@@ -84,6 +84,7 @@ def create_app():
     from .takhfid_v2 import takhfid_v2_bp
     from .takhfid_profile_v2 import takhfid_profile_v2_bp
     from .takhfid_orders_v2 import takhfid_orders_v2_bp
+    from .takhfid_chat_v2 import takhfid_chat_v2_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
@@ -92,6 +93,7 @@ def create_app():
     app.register_blueprint(takhfid_v2_bp)
     app.register_blueprint(takhfid_profile_v2_bp)
     app.register_blueprint(takhfid_orders_v2_bp)
+    app.register_blueprint(takhfid_chat_v2_bp)
 
     @login_manager.user_loader
     def load_user(user_id):
